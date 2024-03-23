63d386ded4c47970c7d595b469a84dc05e539747
zhaixiaohua121 <1959834569@qq.com>
2024-03-23 17:12:06 +0800

    Initial commit

---
+++ vue/vue_projects/vue_test01_Ajax/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+<!--    <h1>{{message}}</h1>-->
+<emp-view></emp-view>
+  </div>
+</template>
+<script>
+  import EmpView from "@/views/tlias/EmpView.vue";
+
+  export default {
+      components: {EmpView},
+      data(){
+          return{
+              message:"hello world"
+          }
+      },
+      methods:{
+      }
+  }
+</script>
+<style>
+
+</style>
